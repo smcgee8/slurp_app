93fb96c03a7456e3678859d532d2dbbd9ccc5990
Sean McGee <sean.a.mcgee@gmail.com>
2017-02-10 00:20:58 -0500

    fix

--- slurp.rb
+++ slurp.rb
@@ -126,6 +126,7 @@ post '/events' do
 
   #Print request headers for retries to console
   puts "Retry Num: #{request["HTTP_X_SLACK_RETRY_NUM"] ||= 0} #{request["HTTP_X_SLACK_RETRY_REASON"]}"
+  puts JSON.pretty_generate(request.env)
 
   #Stop processing if message not verified to be from Slack
   halt 500 if data.token != ENV['SLACK_VERIFICATION_TOKEN']
